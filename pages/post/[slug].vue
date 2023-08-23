6f7b7196a2697dd90b9d5e610adb6eb25074a098
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-08-23 10:35:07 +0600

    Updated

--- pages/post/[slug].vue
+++ pages/post/[slug].vue
@@ -1,11 +1,38 @@
 <template>
     <div>
         <h1>Alhamdulillah. Dynamic Page</h1>
+        <div v-html="body"></div>
     </div>
 </template>
 
 
 <script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const route = useRoute();
+const body = ref();
+
+const getPost = async () => {
+  try {
+    const slug = route.params.slug
+    const response = await axios.get('https://booking.legendaryexoticrentals.com/api/post/get/' + slug);
+    console.log(response.data.body);
+    body.value = response.data.body;
+    // const responseData = response.data['available_cars'];
+  } catch (error) {
+    console.error(error);
+    // Swal.fire('Error', 'Failed to fetch cars', 'error');
+  }
+};
+
+// const getPost = async () => {
+//   console.log(route.params);
+// };
+
+onMounted(getPost);
+
+
 definePageMeta({
   layout: "full-width",
 });
